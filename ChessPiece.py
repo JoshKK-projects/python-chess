49bda34463ac0754c02f174be2144ce945d8a9c3
Emmy CK <ekcalloway@gmail.com>
2016-07-05 15:32:02 -0400

    fixed tab spacing and collision naming
     + fix tab spacing issues in chesspiece init method
     + change naming of collision method to be more clear

--- ChessPiece.py
+++ ChessPiece.py
@@ -1,10 +1,13 @@
 class ChessPiece:
     """ Standard chess piece properties with row, column position """
-    def __init__(self, row, col, team_color)#suggestion - team_color be 1 or -1 so it can be used to determine allowed pawn Y direction
+    #suggestion - team_color be 1 or -1 so it can be used to determine allowed pawn Y direction
+    
+    def __init__(self, row, col, team_color)
     	self.row = row
-	    self.col = col
-	    self.team_color = team_color
-	def possible_move(self,row,col):
+		self.col = col
+		self.team_color = team_color
+	
+	def check_for_collision(self,row,col):
 		if(board[row][col] != 0 and board[row][col].team_color == self.team_color):
 			return(False)
 		if(row>7 or row<0 or col>7 or col<0):
